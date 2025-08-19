1c864b9b982a04892a34ec3f42ec7d04906413d0
BabyWaffles <57724817+BabyWaffles@users.noreply.github.com>
2025-08-19 23:08:28 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CastUnitCircle from './cast_unit_circle';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CastUnitCircle />
+  </React.StrictMode>
+);
